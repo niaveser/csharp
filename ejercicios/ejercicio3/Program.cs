eb000d29c6dbdc71530c82371e04961733d85307
Estefania Robles <l02rosee@gmail.com>
2022-06-26 12:59:37 +0200

    ejercicio 3

---
+++ ejercicios/ejercicio3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+DatosCliente miCliente = new DatosCliente();
+miCliente.ShowClient("Ana López", "666998877", "Calle A", "aaa@gmail.com", true);
+
+
+public struct DatosCliente
+{
+    public DatosCliente(string nombreCompleto, string telefono, string direccion, string correo, Boolean nuevoCliente)
+    {
+        name = nombreCompleto;
+        phone = telefono;
+        address = direccion;
+        email = correo;
+        newClient = nuevoCliente;
+
+    }
+
+    public string name { get; }
+    public string phone { get; }
+    public string address { get; }
+    public string email { get; }
+
+    public Boolean newClient { get; }
+
+    public void ShowClient(string nombreCompleto, string telefono, string direccion, string correo, Boolean nuevoCliente)
+    {
+        Console.WriteLine("Nombre: " + nombreCompleto);
+        Console.WriteLine("Teléfono: " + telefono);
+        Console.WriteLine("Dirección: " + direccion);
+        Console.WriteLine("Email: " + correo);
+        Console.WriteLine("¿Es nuevo?: " + nuevoCliente);
+    }
+
+    
+    public override string ToString()=> $"({name}, {phone}, {address}, {email}, {newClient})";
+    
+}
